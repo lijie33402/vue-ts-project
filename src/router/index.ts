c124b3855330fb4ab490c8a54bb56aeed19a11dd
lijie33402 <823997187@qq.com>
2021-05-10 22:58:11 +0800

    恢复脚手架初始配置状态

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,14 +17,6 @@ const routes: Array<RouteConfig> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/worker',
-    name: 'Worker',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Worker.vue')
   }
 ];
 
--- src/views/Worker.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="worker">
-    <h1>This is an Worker page</h1>
-  </div>
-</template>
